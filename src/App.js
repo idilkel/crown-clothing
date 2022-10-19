bcafbed4e00808274f189ce5473e5e5cd19db6a0
idilkel <96882056+idilkel@users.noreply.github.com>
2022-10-19 18:06:43 +0300

    CartContext, Shopping Icon, Cart Dropdown added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.components";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
-  const Shop = () => {
-    return <h1>I am the Shop page</h1>;
-  };
-
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
